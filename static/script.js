337c41b4313ce94e30e90755947ee4d7c0405437
terrence pietrondi <terrence.a.pietrondi@gmail.com>
2012-04-08 21:14:08 -0400

    removing google logon need and replacing with twitter anywhere

--- static/script.js
+++ static/script.js
@@ -1,3 +1,42 @@
+twttr.anywhere(function (T) {
+    
+    if (T.isConnected()) {
+        twttrAnywhereConnected(T.currentUser);
+        
+        T.linkifyUsers();
+        T.hovercards();
+        
+    } else {
+        $("#twitter-anywhere-needslogin").show();
+        $("#twitter-anywhere-loggedin").hide();
+        
+        T("span#twitter-anywhere-connect").connectButton({ 
+            size: "large",
+            authComplete: function(user) {
+                twttrAnywhereConnected(user);
+            }
+        });
+    }
+    
+});
+
+function twttrAnywhereConnected(currentUser){
+    $("#twitter-anywhere-needslogin").hide();
+    $("#twitter-anywhere-loggedin").show();
+    
+    var screenName,profileImage,profileImageTag;
+    screenName = currentUser.data('screen_name');
+    profileImage = currentUser.data('profile_image_url');
+    
+    $(".twitter-anywhere-screename").html("@" + screenName);
+    
+    $("#twitter-anywhere-signout").bind("click", function () {
+      twttr.anywhere.signOut();
+    });
+    
+    
+}
+
 function twixmitMainReady(){
     window.saveResultsDom = $("#save-results");
     window.textToSave = $("#text-to-save");
