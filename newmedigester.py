e6003b9e8445148aa8a2c7bff3924cf0880b1e60
terrence pietrondi <terrence.a.pietrondi@gmail.com>
2012-05-01 13:27:21 -0400

    adding digest articles

--- newmedigester.py
+++ newmedigester.py
@@ -29,30 +29,31 @@ class NewsMeDigestParser(HTMLParser):
         
         self._tag_states = {"_digest_date" :False, "_digest_articles" : False, "_digest_explore_users" : False}
         
-        #super(D, self).__init__()
-        #def __init__(self):
-        #super(NewsMeDigestParser,self).__init__()
         HTMLParser.__init__(self)
     
     def handle_starttag(self, tag, attrs):
-        #print "Encountered a start tag:", tag, attrs
-    
+        
         href_in_attrs = None
         explore_digest_link_found = False
+        store_link_found = False
                 
         for attr in attrs:
             if tag == 'a' and attr[0] == 'href':
                 href_in_attrs = attr[1]
                 
                 if explore_digest_link_found == True:
-                    print "digest explore user found:", href_in_attrs.strip()
+                    print "digest story link found:", href_in_attrs.strip()
                     self._digest_explore_users.append(href_in_attrs.strip())
                     explore_digest_link_found = False
-                    
+                elif store_link_found == True:
+                    self.handle_start_tag_attr_story_link(href_in_attrs)
+                    store_link_found = False
         
             if tag == 'div' and attr[0] == 'id' and attr[1] == 'digest-date':
                 print "digest date:",attr, type(attr)  
                 self._tag_states["_digest_date"] = True
+            
+            # explore digest links
             elif tag == 'a' and attr[0] == 'class' and attr[1] == 'explore-digest-link' and href_in_attrs == None:
                 explore_digest_link_found = True
             elif tag == 'a' and attr[0] == 'class' and attr[1] == 'explore-digest-link' and href_in_attrs != None:
@@ -60,18 +61,33 @@ class NewsMeDigestParser(HTMLParser):
                 self._digest_explore_users.append(href_in_attrs.strip())
                 explore_digest_link_found = False
             
-        href_in_attrs = None
+            # story links
+            elif tag == 'a' and attr[0] == 'class' and attr[1] == 'story-link' and href_in_attrs != None:
+                self.handle_start_tag_attr_story_link(href_in_attrs)
+                store_link_found = False
             
+        href_in_attrs = None
+    
+    def handle_start_tag_attr_story_link(self,href_in_attrs):
+        print "digest story link found:", href_in_attrs.strip()
+        rec = [href_in_attrs.strip(),None]
+        self._digest_articles.append(rec)
+        self._tag_states["_digest_articles"] = True
         
-    def handle_endtag(self, tag):
-        #print "Encountered an end tag :", tag
-        pass
+    
+    def handle_endtag(self, tag): pass
+        
     def handle_data(self, data):
         if self._tag_states["_digest_date"] == True and self._digest_date == None:
             print "digest date data:",data.strip()
             self._digest_date = data.strip()
             self._tag_states["_digest_date"] == False
-
+        elif self._tag_states["_digest_articles"] == True:
+            print "digest story link date:",data.strip()
+            self._digest_articles[-1][1] = data.strip()
+            self._tag_states["_digest_articles"] = False
+            
+            
 class NewsMeDigester(object):
     def __init__(self,starting_user="tepietrondi",crawl_depth=1):
         self._starting_user = starting_user
