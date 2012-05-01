0ec1af6c86c9fbdd2fbadcd64b2eeb4d0004e67b
terrence pietrondi <terrence.a.pietrondi@gmail.com>
2012-05-01 12:26:55 -0400

    adding a newsme parser to attempt as a twitter feed

---
+++ newmedigester.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from HTMLParser import HTMLParser
+import httplib
+
+class NewsMeDigestParser(HTMLParser):    
+    def __init__(self):
+        #div#id="digest-date", date="May 01, 2012"
+        self._digest_date = None 
+        #a.class="story-link", data-ga-event-action="Top Stories", href=<link>, data=<link title>
+        self._digest_articles = []
+        #a.class="explore-digest-link" href=/<user>
+        self._digest_explore_users = []
+        
+        self._tag_states = {"_digest_date" :False, "_digest_articles" : False, "_digest_explore_users" : False}
+        
+        #super(D, self).__init__()
+        #def __init__(self):
+        #super(NewsMeDigestParser,self).__init__()
+        HTMLParser.__init__(self)
+    
+    def handle_starttag(self, tag, attrs):
+        #print "Encountered a start tag:", tag, attrs
+    
+        href_in_attrs = None
+        explore_digest_link_found = False
+                
+        for attr in attrs:
+            if tag == 'a' and attr[0] == 'href':
+                href_in_attrs = attr[1]
+                
+                if explore_digest_link_found == True:
+                    print "digest explore user found:", href_in_attrs.strip()
+                    self._digest_explore_users.append(href_in_attrs.strip())
+                    explore_digest_link_found = False
+                    
+        
+            if tag == 'div' and attr[0] == 'id' and attr[1] == 'digest-date':
+                print "digest date:",attr, type(attr)  
+                self._tag_states["_digest_date"] = True
+            elif tag == 'a' and attr[0] == 'class' and attr[1] == 'explore-digest-link' and href_in_attrs == None:
+                explore_digest_link_found = True
+            elif tag == 'a' and attr[0] == 'class' and attr[1] == 'explore-digest-link' and href_in_attrs != None:
+                print "digest explore user found:", href_in_attrs.strip()
+                self._digest_explore_users.append(href_in_attrs.strip())
+                explore_digest_link_found = False
+            
+        href_in_attrs = None
+            
+        
+    def handle_endtag(self, tag):
+        #print "Encountered an end tag :", tag
+        pass
+    def handle_data(self, data):
+        if self._tag_states["_digest_date"] == True and self._digest_date == None:
+            print "digest date data:",data.strip()
+            self._digest_date = data.strip()
+            self._tag_states["_digest_date"] == False
+
+class NewsMeDigester(object):
+    def __init__(self,starting_user="tepietrondi",crawl_depth=1):
+        self._starting_user = starting_user
+        self._crawl_depth=crawl_depth
+        self._host = "www.news.me"
+        self._url = "http://%s/%s"
+        
+    def do_digest_digestion(self):
+        digest_data = self.get_digest_page()
+        parser = NewsMeDigestParser()
+        if digest_data != None:
+            parser.feed(digest_data)
+        
+    
+    def get_digest_page(self):
+        conn = httplib.HTTPConnection(self._host)
+        conn.connect()
+        conn.request('GET', self._url % (self._host,self._starting_user) )
+        resp = conn.getresponse()
+        if resp.status != 200:
+            return None
+        else:
+            return resp.read()
+
+def main():
+    digester = NewsMeDigester()
+    digester.do_digest_digestion()
+    
+if __name__ == '__main__':
+    main()
